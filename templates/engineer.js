f5eb6d8f60b949f36cb08afdd71f89c9116c8ae1
ananya92 <1992ananya@gmail.com>
2020-01-25 00:23:11 +1030

    Fixed bug when github username is not available then added the text NA in the HTML element and created the anchor tag only when github id is available

--- templates/engineer.js
+++ templates/engineer.js
@@ -1,11 +1,18 @@
 function getEngineerStr(data) {
+    var github = "";
+    if(data.github !== "NA") {
+      github = `<a href = "https://github.com/${data.github}" class="card-link"><i class="fab fa-github"></i> ${data.github}</a>`;
+    }
+    else {
+      github = `<p class="card-text"><i class="fab fa-github"></i> NA</p>`;
+    }
     return `<div class="col-4" style="width: 18rem;">
       <div class="card mb-3">
         <div class="card-header bg-primary text-white"><h4><i class="fas fa-user-cog"></i> ${data.name}</h4></div>
         <div class="card-body">
           <h6 class="card-text text-muted" style="margin-bottom: 1rem;"><i class="far fa-id-card"></i> ${data.id}</h6>
           <p class="card-text"><i class="far fa-envelope"></i> ${data.email}</p>
-          <a href="https://github.com/${data.github}" class="card-link"><i class="fab fa-github"></i> ${data.github}</a>
+          ${github}
         </div>
       </div>
     </div>`;
